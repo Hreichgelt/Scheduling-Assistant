4c14038fe34d415182145702a9a8d25788d435b0
Hans Reichgelt <hreichgelt06@gmail.com>
2022-05-29 20:10:07 -0400

    adding bootstrap to html and jquery to js

---
+++ Assets/script.js
@@ -0,0 +1,18 @@
+var 
+var tableEL = $('#table');
+var timeEL  = $('#1');
+var activityEL = $('#2');
+var saveEL = $('#3');
+var hours = [
+    '9AM',
+    '10AM',
+    '11AM',
+    'NOON',
+    '1PM',
+    '2PM',
+    '3PM',
+    '4PM',
+    '5PM',
+];
+
+timeEL.each = hours.length
