a300328fe8edf7d86141342c722a5cd8e6d9aa78
Hans Reichgelt <hreichgelt06@gmail.com>
2022-05-30 22:03:22 -0400

    create event listeners functions and if statements for saving activities

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,55 @@
-var 
 var tableEL = $('#table');
 var timeEL  = $('#1');
 var activityEL = $('#2');
-var saveEL = $('#3');
-var hours = [
-    '9AM',
-    '10AM',
-    '11AM',
-    'NOON',
-    '1PM',
-    '2PM',
-    '3PM',
-    '4PM',
-    '5PM',
-];
+var hours = ['9AM', '10AM', '11AM', 'NOON', '1PM', '2PM', '3PM', '4PM','5PM',];
+var position = 0;
+var weekday = $('#currentDay');
+var saveBtn = $('#saveBtn');
+// let todayIs = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][new Date().getDay()]
+// document.write(weekday)
 
-timeEL.each = hours.length
+function insertActivityIntoStorage(hours, activity) {
+    
+}
+
+saveBtn.on('click', function(event) {
+    event.preventDefault();
+    if (activityEL.value === "") {
+        return
+    }
+    saveActivities();
+})
+
+function saveActivities() {
+    var activityEL = activityEL.value.trim();
+    var timeEL = timeEL.value.trim();
+}
+
+
+
+
+// hours.html(function() {
+// time.val()
+// });
+
+// $(timeEL).
+// $(timeEL) = hours.attr();
+
+// $('ul li:first-child') = hours.length;
+
+// timeEL.html = hours.length
+// timeEL.select(hours);
+
+
+// for (var i = 0; i < hours.length; i++) {
+//     timeEL = append.li[0];
+
+// }
+
+// function displayImage() {
+    // var imageEl = document.createElement('img');
+    // var imageURL = './images/one.png'
+    // imageEl.src = imageURL;
+    // imageEl.alt = 'one';
+
+// };
