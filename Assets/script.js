7ab8d5cce78b95269fe305c7dd93f581a6810d88
Hans Reichgelt <hreichgelt06@gmail.com>
2022-06-01 08:20:55 -0400

    refactoring html and css to remove unneccessary items as well as adding comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,30 +1,45 @@
 // global variables with moment.js displaying day date and time
-var rootEl = $('#root');
-var timeEL  = $('.hours');
-var activityEL = $('.textarea');
+// refactoring to clean up variables that are no longer needed
 var saveAllActivitiesAndTimes = [];
-// var position = hours - 9;
-var weekday = $('#currentDay');
 var saveBtn = $('.saveBtn');
-var hours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM','5PM',];
+
+// utilizing link to moment.js - changing format 
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
+var presentTime = moment().hours();
+
+//  when saveBtn is clicked it saves hours and description key value pair into local storage 
+saveBtn.click(function(event) {
+    var clickedBtn = $(event.target);
+    var description = clickedBtn.siblings('.description');
+    var hours = clickedBtn.parent().attr("id");
+    localStorage.setItem(hours, description.val());
+    console.log(hours.val());
+      });
+
+// saving hours and texbox entry into local storage 
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 
-var today = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-console.log(today);
-$("#currentDay").text(today);
-
-// create functions 
+// color-coding for past present future
 function PastPresentFutureColorCoding() {
    var presentTime = moment().hours();
    console.log("Present Time" + presentTime);
 
-    $("textarea").each(function () {
-        var time = parseInt($(this.parent().attr("id").substring(4)));
-        console.log(currentTime, time);
+    $(".description").each(function () {
+        var time = parseInt($(this).parent().attr("id").substring(1, 4));
+        console.log(presentTime, time);
 
-        if (currentTime > time) {
+        if (presentTime > time) {
             $(this).addClass("past")
-        } else if (currentTime < time) {
+        } else if (presentTime < time) {
             $(this).addClass("future");
         } else {
             $(this).addClass("present")
@@ -33,107 +48,5 @@ function PastPresentFutureColorCoding() {
     }
 
  PastPresentFutureColorCoding();
-
-// ------------------------------------
-
-
-//  on button click we want to store the activity into the timeslot object. and then immediately console.log(variable)
-saveBtn.click(function(event) {
-var clickedBtn = $(event.target);
-var textarea = clickedBtn.siblings('.textarea');
-console.log(textarea.val());
-localStorage.setItem(".hours", textarea.val());
-  });
-
-
-
-
-
-// function enterActivitiesIntoLocalStorage(params) {
-    
-// };
-
-
-
-
-
-// Criteria 
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let todayIs = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][new Date().getDay()]
-// document.write(weekday)
-// leaving off on line 11 correlating to line 74 in code quiz
-// for (var i = 0; i < hours.length; i++) {
-//     var hoursEl = $('<li>');
-//     hoursEl.text(hours[i]);
-//     hoursEl.addClass('hours');
-//     rootEl.append(hoursEl);
-// };
-
-// for (let i = 0; i < hours.length; i++) {
-//     var ActivitiesEl = 
-// }
-
-
-// var saveActivitiesAndTimes = JSON.parse(localStorage.getItem(""));
-
-// function insertActivityIntoStorage(hours, activity) {
-//     var row = hour.insertRow();
-//     var cell1 = row.insertCell(0);
-//     var cell2 = row.insertCell(1);
-//     cell1.html = hours;
-//     cell2.html = activityEL;
-// };
-
-// for (let i = 0; i < array.length; i++) {
-//     const element = array[i];
-    
-// };
-
-// saveBtn.on('click', function(event) {
-//     event.preventDefault();
-//     if (activityEL.text === "") {
-//         return
-//     }
-//     saveActivities();
-// });
-
-// function saveActivities() {
-//     var activityEL = activityEL.text.trim();
-//     var timeEL = timeEL.value.trim();
-// };
 
 
